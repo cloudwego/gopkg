843ef58f1234f136be7b56fbd300d074333d9a36
Kyle Xiao <xiaoguoqiao@bytedance.com>
2024-08-12 22:10:34 +0800

    refactor(thrift): compat code no reflect

--- protocol/thrift/apache/apache.go
+++ protocol/thrift/apache/apache.go
@@ -26,206 +26,67 @@
 // Because we're deprecating apache thrift, all kitex ecosystem code will not rely on apache thrift
 // except one pkg: `github.com/cloudwego/kitex/pkg/protocol/bthrift`. Why is the package chosen?
 // All legacy generated code relies on it, and we may not be able to update the code in a brief timeframe.
-// So the package is chosen to register `thrift.NewTBinaryProtocol` to this package in order to use it
+// So the package is chosen to register helper methods to this package in order to use it
 // without importing `github.com/apache/thrift`
 //
-// ThriftRead or ThriftWrite is implemented for calling Read/Write
-// without knowing the interface of `thrift.TProtocol`.
-// Since we already have `thrift.NewTBinaryProtocol`, we only need to check:
-// if the return value of `thrift.NewTBinaryProtocol` implements
-// the input which is `thrift.TProtocol` of Read/Write
+// Users must call `RegisterCheckTStruct`, `RegisterThriftRead` and `RegisterThriftWrite` for
+// using `CheckTStruct`, `ThriftRead`, and `ThriftWrite`
 //
-// For new generated code,
-// it no longer uses the `github.com/cloudwego/kitex/pkg/protocol/bthrift`
+// see README.md of `bthrift` above for more details
 package apache
 
 import (
 	"errors"
-	"fmt"
-	"reflect"
-	"sync"
 )
 
 var (
-	newTBinaryProtocol reflect.Value
+	fnCheckTStruct func(v interface{}) error
 
-	rvTrue = reflect.ValueOf(true) // for calling NewTBinaryProtocol
+	fnThriftRead  func(t TTransport, v interface{}) error
+	fnThriftWrite func(t TTransport, v interface{}) error
 )
-
-var (
-	ttransportType = reflect.TypeOf((*TTransport)(nil)).Elem()
-	errorType      = reflect.TypeOf((*error)(nil)).Elem()
-)
-
-var (
-	errNoNewTBinaryProtocol = errors.New("thrift.NewTBinaryProtocol method not registered. Make sure you're using apache/thrift == 0.13.0 and clouwdwego/kitex >= 0.11.0")
-	errNotPointer           = errors.New("input not pointer")
-	errNoReadMethod         = errors.New("thrift.TStruct `Read` method not found")
-	errNoWriteMethod        = errors.New("thrift.TStruct `Write` method not found")
-
-	errMethodType  = errors.New("method type not match")
-	errNewFuncType = errors.New("function type not match")
-)
-
-func errNewFuncTypeNotMatch(t reflect.Type) error {
-	const expect = "func(thrift.TTransport, bool, bool) *thrift.TBinaryProtocol"
-	return fmt.Errorf("%w:\n\texpect: %s\n\t   got: %s", errNewFuncType, expect, t)
-}
-
-func errReadWriteMethodNotMatch(t reflect.Type) error {
-	const expect = "func(thrift.TProtocol) error"
-	return fmt.Errorf("%w:\n\texpect: %s\n\t   got: %s", errMethodType, expect, t)
-}
 
 // RegisterNewTBinaryProtocol accepts `thrift.NewTBinaryProtocol` func and save it for later use.
-func RegisterNewTBinaryProtocol(fn interface{}) error {
-	v := reflect.ValueOf(fn)
-	t := v.Type()
-
-	// check it's func
-	if t.Kind() != reflect.Func {
-		return errNewFuncTypeNotMatch(t)
-	}
-
-	// check "func(thrift.TTransport, bool, bool) *thrift.TBinaryProtocol"
-	// can also check with t.String() instead of field by field?
-	if t.NumIn() != 3 ||
-		!t.In(0).Implements(ttransportType) ||
-		t.In(1).Kind() != reflect.Bool ||
-		t.In(2).Kind() != reflect.Bool {
-		return errNewFuncTypeNotMatch(t)
-	}
-	if t.NumOut() != 1 {
-		// not checking if it's thrift.TProtocol
-		// but in ThriftRead/ThriftWrite, we will check if it implements the input of Read/Write
-		// so we can make it easier to test.
-		return errNewFuncTypeNotMatch(t)
-	}
-	newTBinaryProtocol = v
-	hasThriftRead = sync.Map{}
-	hasThriftWrite = sync.Map{}
-	return nil
+func RegisterCheckTStruct(fn func(v interface{}) error) {
+	fnCheckTStruct = fn
 }
 
-func checkThriftReadWriteFuncType(t reflect.Type) error {
-	if !newTBinaryProtocol.IsValid() {
-		return errNoNewTBinaryProtocol
-	}
-
-	// checks `func(thrift.TProtocol) error`
-	if t.NumIn() != 1 || t.In(0).Kind() != reflect.Interface ||
-		!newTBinaryProtocol.Type().Out(0).Implements(t.In(0)) {
-		return errReadWriteMethodNotMatch(t)
-	}
-	if t.NumOut() != 1 ||
-		!t.Out(0).Implements(errorType) {
-		return errReadWriteMethodNotMatch(t)
-	}
-	return nil
+// RegisterThriftRead ...
+func RegisterThriftRead(fn func(t TTransport, v interface{}) error) {
+	fnThriftRead = fn
 }
 
-var hasThriftRead = sync.Map{}
-
-// CheckThriftRead returns nil if v has Read method and matches the func signature
-func CheckThriftRead(v interface{}) error {
-	rv := reflect.ValueOf(v)
-	rt := rv.Type()
-	res, ok := hasThriftRead.Load(rt)
-	if ok {
-		// fast path
-		if res == nil {
-			return nil
-		}
-		return res.(error)
-	}
-	if rv.Kind() != reflect.Ptr {
-		hasThriftRead.Store(rt, errNotPointer)
-		return errNotPointer
-	}
-	fv := rv.MethodByName("Read")
-	if !fv.IsValid() {
-		hasThriftRead.Store(rt, errNoReadMethod)
-		return errNoReadMethod
-	}
-	if err := checkThriftReadWriteFuncType(fv.Type()); err != nil {
-		hasThriftRead.Store(rt, err)
-		return err
-	}
-	hasThriftRead.Store(rt, nil)
-	return nil
+// RegisterThriftWrite ...
+func RegisterThriftWrite(fn func(t TTransport, v interface{}) error) {
+	fnThriftWrite = fn
 }
 
-var hasThriftWrite = sync.Map{}
+var (
+	errCheckTStructNotRegistered = errors.New("func `RegisterCheckTStruct` not called")
+	errThriftReadNotRegistered   = errors.New("func `RegisterThriftRead` not called")
+	errThriftWriteNotRegistered  = errors.New("func `RegisterThriftWrite` not called")
+)
 
-// CheckThriftWrite returns nil if v has Write method and matches the func signature
-func CheckThriftWrite(v interface{}) error {
-	rv := reflect.ValueOf(v)
-	rt := rv.Type()
-	res, ok := hasThriftWrite.Load(rt)
-	if ok {
-		// fast path
-		if res == nil {
-			return nil
-		}
-		return res.(error)
+// CheckTStruct ...
+func CheckTStruct(v interface{}) error {
+	if fnCheckTStruct == nil {
+		return errCheckTStructNotRegistered
 	}
-	if rv.Kind() != reflect.Ptr {
-		hasThriftWrite.Store(rt, errNotPointer)
-		return errNotPointer
-	}
-	fv := rv.MethodByName("Write")
-	if !fv.IsValid() {
-		hasThriftWrite.Store(rt, errNoWriteMethod)
-		return errNoWriteMethod
-	}
-	if err := checkThriftReadWriteFuncType(fv.Type()); err != nil {
-		hasThriftWrite.Store(rt, err)
-		return err
-	}
-	hasThriftWrite.Store(rt, nil)
-	return nil
+	return fnCheckTStruct(v)
 }
 
-// ThriftRead calls Read method of v.
-//
-// RegisterNewTBinaryProtocol must be called with `thrift.NewTBinaryProtocol`
-// before using this func.
+// ThriftRead ...
 func ThriftRead(t TTransport, v interface{}) error {
-	if err := CheckThriftRead(v); err != nil {
-		return err
+	if fnThriftRead == nil {
+		return errThriftReadNotRegistered
 	}
-
-	// iprot := NewTBinaryProtocol(t, true, true)
-	iprot := newTBinaryProtocol.Call([]reflect.Value{reflect.ValueOf(t), rvTrue, rvTrue})[0]
-
-	// err := v.Read(iprot)
-	rv := reflect.ValueOf(v)
-	rfunc := rv.MethodByName("Read")
-	err := rfunc.Call([]reflect.Value{iprot})[0]
-	if err.IsNil() {
-		return nil
-	}
-	return err.Interface().(error)
+	return fnThriftRead(t, v)
 }
 
-// ThriftWrite calls Write method of v.
-//
-// RegisterNewTBinaryProtocol must be called with `thrift.NewTBinaryProtocol`
-// before using this func.
+// ThriftWrite ...
 func ThriftWrite(t TTransport, v interface{}) error {
-	if err := CheckThriftWrite(v); err != nil {
-		return err
+	if fnThriftWrite == nil {
+		return errThriftWriteNotRegistered
 	}
-
-	// oprot := NewTBinaryProtocol(t, true, true)
-	oprot := newTBinaryProtocol.Call([]reflect.Value{reflect.ValueOf(t), rvTrue, rvTrue})[0]
-
-	// err := v.Write(oprot)
-	rv := reflect.ValueOf(v)
-	wfunc := rv.MethodByName("Write")
-	err := wfunc.Call([]reflect.Value{oprot})[0]
-	if err.IsNil() {
-		return nil
-	}
-	return err.Interface().(error)
+	return fnThriftWrite(t, v)
 }
--- protocol/thrift/apache/apache_test.go
+++ protocol/thrift/apache/apache_test.go
@@ -21,169 +21,81 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
-	"reflect"
-	"sync"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestRegisterNewTBinaryProtocol(t *testing.T) {
-	{ // case: not func type
-		fn := 1
-		err := RegisterNewTBinaryProtocol(fn)
-		t.Log(err)
-		assert.ErrorIs(t, err, errNewFuncType)
-	}
+func TestThriftReadWrite(t *testing.T) {
 
-	{ // case: args err
-		fn := func(_ TTransport, _ bool, _ int) {}
-		err := RegisterNewTBinaryProtocol(fn)
-		t.Log(err)
-		assert.ErrorIs(t, err, errNewFuncType)
-	}
+	v := &TestingWriteRead{Msg: "Hello"}
 
-	{ // case: ret err
-		fn := func(_ TTransport, _, _ bool) {}
-		err := RegisterNewTBinaryProtocol(fn)
-		t.Log(err)
-		assert.ErrorIs(t, err, errNewFuncType)
-	}
+	err := CheckTStruct(v)
+	require.Same(t, err, errCheckTStructNotRegistered)
+	RegisterCheckTStruct(checkTStruct)
+	err = CheckTStruct(v)
+	require.NoError(t, err)
 
-	{ // case: no err
-		fn := func(_ TTransport, _, _ bool) error { return nil }
-		err := RegisterNewTBinaryProtocol(fn)
-		assert.NoError(t, err)
-		assert.True(t, newTBinaryProtocol.IsValid())
-		newTBinaryProtocol = reflect.Value{} // reset
-	}
+	buf := &bytes.Buffer{}
+
+	err = ThriftWrite(NewBufferTransport(buf), v)
+	require.Same(t, err, errThriftWriteNotRegistered)
+
+	RegisterThriftWrite(callThriftWrite)
+	err = ThriftWrite(NewBufferTransport(buf), v) // calls v.Write
+	require.NoError(t, err)
+
+	p := &TestingWriteRead{}
+
+	err = ThriftRead(NewBufferTransport(buf), p)
+	require.Same(t, err, errThriftReadNotRegistered)
+
+	RegisterThriftRead(callThriftRead)
+	err = ThriftRead(NewBufferTransport(buf), p) // calls p.Read
+	require.NoError(t, err)
+
+	require.Equal(t, v.Msg, p.Msg)
+}
+
+type TStruct interface { // simulate thrift.TStruct
+	Read(r io.Reader) error
+	Write(w io.Writer) error
 }
 
 type TestingWriteRead struct {
 	Msg string
-
-	mockErr error
 }
 
 func (t *TestingWriteRead) Read(r io.Reader) error {
-	if t.mockErr != nil {
-		return t.mockErr
-	}
 	return json.NewDecoder(r).Decode(t)
 }
 
 func (t *TestingWriteRead) Write(w io.Writer) error {
-	if t.mockErr != nil {
-		return t.mockErr
-	}
 	return json.NewEncoder(w).Encode(t)
 }
 
-func TestThriftWriteRead(t *testing.T) {
-	called := 0
-	fn := func(trans TTransport, b0, b1 bool) *bytes.Buffer {
-		assert.True(t, b0)
-		assert.True(t, b1)
-		called++
-		return &trans.(*bufferTransport).Buffer
-	}
-	err := RegisterNewTBinaryProtocol(fn)
-	require.NoError(t, err)
-	defer func() { newTBinaryProtocol = reflect.Value{} }()
+var errNotThriftTStruct = errors.New("errNotThriftTStruct")
 
-	expectcalls := 0
-	for i := 0; i < 2; i++ { // run twice to test cache
-		buf := &bytes.Buffer{}
-		p0 := &TestingWriteRead{Msg: "hello"}
-		err = ThriftWrite(NewBufferTransport(buf), p0) // calls p0.Write
-		require.NoError(t, err)
-		expectcalls++
-		require.Equal(t, expectcalls, called)
-
-		p1 := &TestingWriteRead{}
-		err = ThriftRead(NewBufferTransport(buf), p1) // calls p1.Read
-		require.NoError(t, err)
-		expectcalls++
-		require.Equal(t, expectcalls, called)
-		require.Equal(t, p0, p1)
+func checkTStruct(v interface{}) error {
+	_, ok := v.(TStruct)
+	if !ok {
+		return errNotThriftTStruct
 	}
+	return nil
 }
 
-type TestingWriteReadMethodNotMatch struct{}
-
-func (_ *TestingWriteReadMethodNotMatch) Read(v bool) error  { return nil }
-func (_ *TestingWriteReadMethodNotMatch) Write(v bool) error { return nil }
-
-type TestingNoReadWriteMethod struct{}
-
-func (_ *TestingNoReadWriteMethod) Read1(v bool) error  { return nil }
-func (_ *TestingNoReadWriteMethod) Write1(v bool) error { return nil }
-
-type TestingWriteReadNotReturningErr struct{}
-
-func (_ *TestingWriteReadNotReturningErr) Read(r io.Reader)  {}
-func (_ *TestingWriteReadNotReturningErr) Write(w io.Writer) {}
-
-func TestCheckThriftReadWriteErr(t *testing.T) {
-	// reset type cache
-	hasThriftRead = sync.Map{}
-	hasThriftWrite = sync.Map{}
-
-	var err error
-
-	// errNotPointer
-	for i := 0; i < 2; i++ { // run twice to test cache
-		err = CheckThriftRead(TestingWriteRead{})
-		assert.Same(t, err, errNotPointer)
-		err = CheckThriftWrite(TestingWriteRead{})
-		assert.Same(t, err, errNotPointer)
+func callThriftRead(t TTransport, v interface{}) error {
+	p, ok := v.(TStruct)
+	if !ok {
+		return errNotThriftTStruct
 	}
-
-	// errNoNewTBinaryProtocol
-	for i := 0; i < 2; i++ { // run twice to test cache
-		err = CheckThriftRead(&TestingWriteRead{})
-		assert.Same(t, err, errNoNewTBinaryProtocol)
-		err = CheckThriftWrite(&TestingWriteRead{})
-		assert.Same(t, err, errNoNewTBinaryProtocol)
-	}
-
-	fn := func(trans TTransport, b0, b1 bool) *bytes.Buffer { return nil }
-	RegisterNewTBinaryProtocol(fn)
-	defer func() { newTBinaryProtocol = reflect.Value{} }()
-
-	// errMethodType
-	for i := 0; i < 2; i++ {
-		err = CheckThriftRead(&TestingWriteReadMethodNotMatch{}) // input type err
-		assert.ErrorIs(t, err, errMethodType)
-		err = CheckThriftWrite(&TestingWriteReadMethodNotMatch{})
-		assert.ErrorIs(t, err, errMethodType)
-		err = CheckThriftRead(&TestingWriteReadNotReturningErr{}) // return type err
-		assert.ErrorIs(t, err, errMethodType)
-		err = CheckThriftWrite(&TestingWriteReadNotReturningErr{})
-		assert.ErrorIs(t, err, errMethodType)
-	}
-
-	// errNoReadMethod, errNoWriteMethod
-	for i := 0; i < 2; i++ {
-		err = CheckThriftRead(&TestingNoReadWriteMethod{})
-		assert.ErrorIs(t, err, errNoReadMethod)
-		err = CheckThriftWrite(&TestingNoReadWriteMethod{})
-		assert.ErrorIs(t, err, errNoWriteMethod)
-	}
+	return p.Read(t)
 }
 
-func TestThriftWriteReadErr(t *testing.T) {
-	var err error
-
-	// Read/Write returns err
-	p := TestingWriteRead{}
-	fn := func(trans TTransport, b0, b1 bool) *bytes.Buffer { return nil }
-	RegisterNewTBinaryProtocol(fn)
-	defer func() { newTBinaryProtocol = reflect.Value{} }()
-	p.mockErr = errors.New("mock")
-	err = ThriftWrite(NewBufferTransport(nil), &p)
-	assert.Same(t, err, p.mockErr)
-	err = ThriftRead(NewBufferTransport(nil), &p)
-	assert.Same(t, err, p.mockErr)
+func callThriftWrite(t TTransport, v interface{}) error {
+	p, ok := v.(TStruct)
+	if !ok {
+		return errNotThriftTStruct
+	}
+	return p.Write(t)
 }
