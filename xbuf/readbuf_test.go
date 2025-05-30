28aa45ed33505f38a68b0ea193b765163e40fb22
jayantxie <xiezhengyao@bytedance.com>
2025-05-30 11:48:15 +0800

    feat: xbuffer

--- xbuf/readbuf_test.go
+++ xbuf/readbuf_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestReadBuf(t *testing.T) {
+func TestReadBuf_Inline(t *testing.T) {
 	var x *XReadBuffer
 
 	defer func() {
@@ -33,4 +33,99 @@ func TestReadBuf(t *testing.T) {
 	}()
 
 	x.CopyBytes(make([]byte, 1))
+}
+
+func TestReadBuf_CrossPad(t *testing.T) {
+	tf := func(getBuf func(x *XReadBuffer, n int) []byte) {
+		ori1 := make([]byte, padLength)
+		for i := range ori1 {
+			ori1[i] = 'a'
+		}
+		ori2 := make([]byte, padLength)
+		for i := range ori2 {
+			ori2[i] = 'b'
+		}
+		ori := [][]byte{ori1, ori2}
+		x := NewXReadBuffer(ori)
+		defer x.Free()
+		buf := getBuf(x, padLength-1)
+		if len(buf) < padLength-1 {
+			t.Fatal("buf length should be great or equal to padLength-1")
+		}
+		for _, byt := range buf[:padLength-1] {
+			if byt != 'a' {
+				t.Fatal("byt should be 'a'")
+			}
+		}
+		buf = getBuf(x, 2)
+		if len(buf) < 2 {
+			t.Fatal("buf length should be great or equal to 2")
+		}
+		if buf[0] != 'a' {
+			t.Fatal("buf[0] should be 'a'")
+		}
+		if buf[1] != 'b' {
+			t.Fatal("buf[1] should be 'b'")
+		}
+		buf = getBuf(x, padLength-1)
+		if len(buf) < padLength-1 {
+			t.Fatal("buf length should be great or equal to padLength-1")
+		}
+		for _, byt := range buf[:padLength-1] {
+			if byt != 'b' {
+				t.Fatal("byt should be 'b'")
+			}
+		}
+	}
+	tf(func(x *XReadBuffer, n int) []byte {
+		return x.ReadN(n)
+	})
+	tf(func(x *XReadBuffer, n int) []byte {
+		buf := make([]byte, n)
+		x.CopyBytes(buf)
+		return buf
+	})
+}
+
+func TestReadBuf_NoCrossPad(t *testing.T) {
+	tf := func(getBuf func(x *XReadBuffer, n int) []byte) {
+		ori1 := make([]byte, padLength/2)
+		for i := range ori1 {
+			ori1[i] = 'a'
+		}
+		ori2 := make([]byte, padLength/2)
+		for i := range ori2 {
+			ori2[i] = 'b'
+		}
+		ori := append(ori1, ori2...)
+		x := NewXReadBuffer([][]byte{ori})
+		defer x.Free()
+
+		buf := getBuf(x, padLength/2)
+		if len(buf) < padLength/2 {
+			t.Fatal("buf length should be great or equal to padLength/2")
+		}
+		for _, byt := range buf[:padLength/2] {
+			if byt != 'a' {
+				t.Fatal("byt should be 'a'")
+			}
+		}
+		buf = getBuf(x, padLength/2)
+		if len(buf) < padLength/2 {
+			t.Fatal("buf length should be great or equal to padLength/2")
+		}
+		for _, byt := range buf[:padLength/2] {
+			if byt != 'b' {
+				t.Fatal("byt should be 'b'")
+			}
+		}
+	}
+	tf(func(x *XReadBuffer, n int) []byte {
+		return x.ReadN(n)
+	})
+	tf(func(x *XReadBuffer, n int) []byte {
+		buf := make([]byte, n)
+		x.CopyBytes(buf)
+		return buf
+	})
 }
--- xbuf/writebuf_test.go
+++ xbuf/writebuf_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestXWriteBuffer(t *testing.T) {
+func TestXWriteBuffer_Inline(t *testing.T) {
 	var b *XWriteBuffer
 
 	defer func() {
@@ -21,4 +21,95 @@ func TestXWriteBuffer(t *testing.T) {
 	}()
 
 	b.MallocN(10)
+}
+
+func TestXWriteBuffer_CrossPad(t *testing.T) {
+	b := NewXWriteBuffer()
+	defer b.Free()
+	buf := b.MallocN(padLength - 1)
+	for i := range buf {
+		buf[i] = 'a'
+	}
+	buf = b.MallocN(2)
+	for i := range buf {
+		buf[i] = 'b'
+	}
+	bytes := b.Bytes()
+	if len(bytes) != 2 {
+		t.Fatal("bytes length should be 2")
+	}
+	if len(bytes[0]) != padLength-1 {
+		t.Fatal("bytes[0] length should be padLength-1")
+	}
+	for i := range bytes[0] {
+		if bytes[0][i] != 'a' {
+			t.Fatal("bytes[0][i] should be 'a'")
+		}
+	}
+	if len(bytes[1]) != 2 {
+		t.Fatal("bytes[1] length should be 2")
+	}
+	for i := range bytes[1] {
+		if bytes[1][i] != 'b' {
+			t.Fatal("bytes[1][i] should be 'b'")
+		}
+	}
+}
+
+func TestXWriteBuffer_NoCrossPad(t *testing.T) {
+	b := NewXWriteBuffer()
+	defer b.Free()
+	buf := b.MallocN(1024)
+	for i := range buf {
+		buf[i] = 'a'
+	}
+	buf = b.MallocN(1024)
+	for i := range buf {
+		buf[i] = 'b'
+	}
+	bytes := b.Bytes()
+	if len(bytes) != 1 {
+		t.Fatal("bytes length should be 1")
+	}
+	if len(bytes[0]) != 2048 {
+		t.Fatal("bytes[0] length should be 2048")
+	}
+	for i := range bytes[0] {
+		if i < 1024 && bytes[0][i] != 'a' {
+			t.Fatal("bytes[0][i] should be 'a'")
+		}
+		if i >= 1024 && bytes[0][i] != 'b' {
+			t.Fatal("bytes[0][i] should be 'b'")
+		}
+	}
+}
+
+func TestXWriteBuffer_WriteDirect(t *testing.T) {
+	b := NewXWriteBuffer()
+	defer b.Free()
+	buf := b.MallocN(1024)
+	for i := range buf {
+		buf[i] = 'a'
+	}
+	b.WriteDirect([]byte{'b', 'c'})
+	bytes := b.Bytes()
+	if len(bytes) != 2 {
+		t.Fatal("bytes length should be 2")
+	}
+	if len(bytes[0]) != 1024 {
+		t.Fatal("bytes[0] length should be 1024")
+	}
+	if len(bytes[1]) != 2 {
+		t.Fatal("bytes[1] length should be 2")
+	}
+	for i := range bytes[0] {
+		if bytes[0][i] != 'a' {
+			t.Fatal("bytes[0][i] should be 'a'")
+		}
+	}
+	for i := range bytes[1] {
+		if bytes[1][i] != byte('b'+i) {
+			t.Fatal("bytes[1][i] should be 'b'+i")
+		}
+	}
 }
